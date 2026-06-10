4d50092976941a488266ec497d153f5905e18c4b
Anton Vishneuski <anton.vishneuskij@gmail.com>
2026-06-10 21:00:22 +0200

    feat: sitemap includes published service pages (hreflang)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,12 @@
 import type { MetadataRoute } from 'next'
+import { getServicePageParams } from '@/lib/queries'
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const url = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
-  const paths = ['', '/polityka-prywatnosci']
+  const staticPaths = ['', '/polityka-prywatnosci']
+  const slugs = await getServicePageParams()
+  const servicePaths = slugs.map((s) => `/uslugi/${s}`)
+  const paths = [...staticPaths, ...servicePaths]
   return paths.flatMap((p) => {
     const languages = { pl: `${url}${p}`, ru: `${url}/ru${p}`, 'x-default': `${url}${p}` }
     return [
